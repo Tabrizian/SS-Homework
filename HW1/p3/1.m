6f21608d7d2919194746a2042306954297682f52
Iman Tabrizian <Tabrizian@outlook.com>
2017-02-25 23:53:12 +0330

    Adds question 3 solution

---
+++ HW1/p3/1.m
@@ -0,0 +1,12 @@
+pkg load specfun
+function plot_function()
+    w0 = [pi/8 pi/4 pi/2 pi 5*pi/4 3*pi/2 2*pi];
+    n = -5:1:15;
+    for i = 1:7
+        x = sin(n * 2 * pi);
+        subplot(3, 3, i);
+       stem(n, x);
+    endfor
+    input("Salam");
+endfunction
+plot_function()
